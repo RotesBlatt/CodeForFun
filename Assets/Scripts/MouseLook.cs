139131f27c56b313c94c4c28488620f3ee3eb6cd
RotesBlatt <RotesBlatt@users.noreply.github.com>
2021-02-11 13:45:14 +0100

    Finisched Movement
    Alles außer Sprint funktioneiert

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
 
-    public float mouseSensitivity = 150f;
+    public float mouseSensitivity = 500f;
 
     public Transform playerBody;
 
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,26 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float sprintSpeed = 100f;
     public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
     Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = 0f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -21,9 +34,14 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
         velocity.y += gravity * Time.deltaTime;
 
-        controller.Move(velocity);
+        controller.Move(velocity * Time.deltaTime);
+
         
     }
 }
