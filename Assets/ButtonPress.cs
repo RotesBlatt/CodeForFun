04c21ce6452344225d0b1cf980b0583e26086861
LucioRoyal2BH <65733380+LucioRoyal2BH@users.noreply.github.com>
2021-02-12 15:43:35 +0100

    Button
    Buttons and doors

---
+++ Assets/ButtonPress.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    bool isInRange;
+    public bool buttonPressed;
+    public int ID;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject gameObject = GameObject.Find("Door");
+        Door pressButton = gameObject.GetComponent<Door>();
+
+        if (isInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Button Pressed");
+
+            pressButton.DeleteDoor(ID);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "First Person Player")
+        {
+            Debug.Log("alla ez");
+            isInRange = true;
+        }
+    }
+
+      private void OnTriggerStay(Collider other) {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isInRange = false;
+    }
+}
